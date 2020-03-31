c763cf8787f124c4345a9ef9b1336daf06f6f9b8
bendevera <ben10devera@gmail.com>
2020-03-31 14:04:45 -0600

    ben: adjusted how row is converted to model instance

--- api/utils/writer/writers.py
+++ api/utils/writer/writers.py
@@ -19,20 +19,7 @@ def write_trial(data: pd.DataFrame):
     """
     with get_session() as session:
         for i in range(data.shape[0]):
-            curr_trial = TrialRaw(
-                title=data.loc['title', i],
-                registry=data.loc['registry', i],
-                registration_date=data.loc['registration_date', i],
-                enrollment_date=data.loc['enrollment_date', i],
-                start_date=data.loc['start_date', i],
-                recruitment_status=data.loc['recruitment_status', i],
-                intervention_type=data.loc['intervention_type', i],
-                sponsors=data.loc['sponsors', i],
-                countries=data.loc['countries', i],
-                data_reference=data.loc['data_reference', i],
-                data_source=data.loc['data_source', i],
-                results_link=data.loc['results_link', i]
-            )
+            curr_trial = TrialRaw(data.iloc[i].to_dict())
             session.add(curr_trial)
         session.commit()
 
@@ -43,26 +30,6 @@ def write_product(data: pd.DataFrame):
     """
     with get_session() as session:
         for i in range(data.shape[0]):
-            curr_product = ProductRaw(
-                preferred_name=data.loc['preferred_name', i],
-                chemical_name=data.loc['chemical_name', i],
-                brand_name=data.loc['brand_name', i],
-                repurposed=data.loc['repurposed', i],
-                notes=data.loc['notes', i],
-                disease=data.loc['disease', i],
-                application=data.loc['application', i],
-                product_type=data.loc['product_type', i], 
-                indication=data.loc['indication', i],
-                molecule_type=data.loc['molecule_type', i],
-                therapeutic_approach=data.loc['therapeutic_approach', i],
-                other_partners=data.loc['other_partners', i],
-                num_sites=data.loc['num_sites', i],
-                site_locations=data.loc['site_locations', i],
-                enrollment_date=data.loc['enrollment_date', i],
-                intervention_type=data.loc['intervention_type', i],
-                sponsors=data.loc['sponsors', i],
-                data_reference=data.loc['data_reference', i],
-                data_source=data.loc['data_source', i]
-            )
+            curr_product = ProductRaw(data.iloc[i].to_dict())
             session.add(curr_product)
         session.commit()
