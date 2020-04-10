1211a98790506022cf88714cec6296cb66160e35
newnativeabq <vincent.a.brandon@gmail.com>
2020-04-10 16:29:34 -0600

    working on raw cleaner

--- worker.py
+++ worker.py
@@ -5,6 +5,10 @@ from rq import Worker, Queue, Connection
 
 from decouple import config
 
+import logging
+
+worker_logger = logging.getLogger(__name__)
+
 listen = ['default']
 
 redis_url = config('REDISTOGO_URL', default='redis://localhost:6379')
@@ -13,5 +17,6 @@ conn = redis.from_url(redis_url)
 
 if __name__ == '__main__':
     with Connection(conn):
+        worker_logger.info('Starting redis worker listening.')
         worker = Worker(list(map(Queue, listen)))
         worker.work()
