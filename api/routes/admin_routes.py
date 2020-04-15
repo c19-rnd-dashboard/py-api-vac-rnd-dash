1e49f686767a4ea7c7a12d8c4e1a9304d9122dfd
newnativeabq <vincent.a.brandon@gmail.com>
2020-04-14 20:06:13 -0600

    ENH: updated static trial route to newer file

--- api/routes/admin_routes.py
+++ api/routes/admin_routes.py
@@ -95,7 +95,7 @@ def run_database_update():
         {'loader': "unfiltered_csv"}
         ),
         ('trial', 
-        'https://raw.githubusercontent.com/ebmdatalab/covid_trials_tracker-covid/master/notebooks/processed_data_sets/trial_list_2020-03-25.csv',
+        'https://raw.githubusercontent.com/ebmdatalab/covid_trials_tracker-covid/master/notebooks/processed_data_sets/trial_list_2020-04-07.csv',
         {}
         ),
         ('milestone',
