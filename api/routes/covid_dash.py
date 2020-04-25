a19222358ad72b3ad1ae0d60944175c36af85d17
newnativeabq <vincent.a.brandon@gmail.com>
2020-04-24 22:00:41 -0600

    ENH: refactored asset endpoint for more efficient db usage 2 queries vs n

--- api/routes/covid_dash.py
+++ api/routes/covid_dash.py
@@ -59,6 +59,26 @@ def products():
     return jsonify([med.to_json() for med in meds])
 
 
+
+def condense_sponsors(sponsor_results):
+    # Requires sponsors in the form (product_id, sponsor_id, sponsor_name)
+    unique_ids = sorted(list(set(result[0] for result in sponsor_results)))
+    sponsor_dict= {}
+    [sponsor_dict.update({unique_id:[]}) for unique_id in unique_ids]
+    for result in sponsor_results:
+        prod_id = result[0]
+        spon_id = result[1]
+        spon_name = result[2]
+        sponsor_info = {'sponsorId': spon_id, 'sponsorName': spon_name}
+        sponsor_dict[prod_id] = sponsor_dict[prod_id] + [sponsor_info]
+    return sponsor_dict
+
+def fetch_value(dictionary, key):
+    if key in dictionary:
+        return dictionary[key]
+    else:
+        return []
+
 @covid_dash.route("/assets")
 def assets():
     routelogger.info("Running Products Query")
@@ -68,13 +88,15 @@ def assets():
         # Serialize the assets 
         serialized_assets = [ccase_serializer.transform(item.json) for item in assets]
         # Bring in other data via joins and lookups
-            # Sponsors #
+        # Sponsors #
+        sponsors_info = session.query(ProductSponsor.product_id, Sponsor.sponsor_id, Sponsor.sponsor_name).join(ProductSponsor).all()
+        sponsors = condense_sponsors(sponsors_info)
+
         for asset in serialized_assets:
-            asset_sponsors = [
-                result.json \
-                for result in \
-                session.query(Sponsor).join(ProductSponsor).\
-                    filter(ProductSponsor.product_id == asset['productId']).all()
-            ]
+            asset_sponsors = fetch_value(sponsors, asset['productId'])
             asset['sponsors'] = asset_sponsors
+            asset['sources'] = asset['sources'].split(',')
     return jsonify(serialized_assets)
+
+
+    
