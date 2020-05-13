3929c5a5a91245f2dd72feb07f3a1212d901bd30
newnativeabq <vincent.a.brandon@gmail.com>
2020-05-12 18:32:24 -0600

    UPDATE: Move vaccine default file to more recent v3

--- api/routes/admin_routes.py
+++ api/routes/admin_routes.py
@@ -101,7 +101,7 @@ def run_database_update():
          {}
          ),
         ('product',
-         'https://raw.githubusercontent.com/c19-rnd-dashboard/py-api-vac-rnd-dash/master/data/vaccines/vaccineworkfile2.csv',
+         'https://raw.githubusercontent.com/c19-rnd-dashboard/py-api-vac-rnd-dash/master/data/vaccines/vaccineworkfile3.csv',
          {'loader': "unfiltered_csv"}
          ),
         ('trial',
--- tests/test_utils.py
+++ tests/test_utils.py
@@ -18,7 +18,7 @@ class IngestTest(LiveServerTestCase):
 
     def setUp(self):
         self.test_trial_url = 'https://raw.githubusercontent.com/ebmdatalab/covid_trials_tracker-covid/master/notebooks/processed_data_sets/trial_list_2020-03-25.csv'
-        self.test_product_url = 'https://raw.githubusercontent.com/c19-rnd-dashboard/py-api-vac-rnd-dash/master/data/vaccines/vaccineworkfile2.csv'
+        self.test_product_url = 'https://raw.githubusercontent.com/c19-rnd-dashboard/py-api-vac-rnd-dash/master/data/vaccines/vaccineworkfile3.csv'
         self.test_factory_milestones = factory_milestones
         self.test_factory_countries = factory_countries
 
