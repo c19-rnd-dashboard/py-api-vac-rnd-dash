82e11338c06d83d7ae3f41eebb15ee16ae0541c3
newnativeabq <vincent.a.brandon@gmail.com>
2020-06-06 11:23:41 -0600

    ENH: deprecation comment renumber_id

--- api/utils/transform/products/product_transforms.py
+++ api/utils/transform/products/product_transforms.py
@@ -14,17 +14,18 @@ logger = logging.getLogger(__name__)
 ######################################
 
 
-def renumber_id(data: pd.DataFrame, id_col='ID') -> pd.DataFrame:
-    logger.info(f"Starting id renumber on {id_col}")
-    id_list = []
-    id = 1
-    for i, _ in enumerate(range(len(data))):
-        id_list.append(id)
-        if i%2 != 0:
-            id += 1
+## DEPRECATED - REMOVE IN FUTURE ITERATION ##
+# def renumber_id(data: pd.DataFrame, id_col='ID') -> pd.DataFrame:
+#     logger.info(f"Starting id renumber on {id_col}")
+#     id_list = []
+#     id = 1
+#     for i, _ in enumerate(range(len(data))):
+#         id_list.append(id)
+#         if i%2 != 0:
+#             id += 1
 
-    data['ID'] = id_list
-    return data
+#     data['ID'] = id_list
+#     return data
 
 
 def clean_product_raw(data: pd.DataFrame):
