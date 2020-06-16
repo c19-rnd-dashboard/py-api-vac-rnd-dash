7686f120473a3e0269956465149cdbe2cbc2b442
newnativeabq <vincent.a.brandon@gmail.com>
2020-06-16 06:58:43 -0600

    FIX: updated geolocation imports to alleviate naming collision

--- api/utils/transform/products/product_geolocation.py
+++ api/utils/transform/products/product_geolocation.py
@@ -17,10 +17,20 @@ def _add_index_as_col(dataframe:pd.DataFrame, col_name) -> pd.DataFrame:
     temp[col_name] = np.array(temp.index.values)
     return temp
 
+
+def _lookup_alpha3_countrycode(code):
+    pass
+
+
+
 def prep_product_sitelocation(data: pd.DataFrame):
     tlogg.info("Starting prep_product_sitelocations")
     tlogg.info(
         f"Transforming frame of shape {data.shape} and columns {data.columns}")
     ndata = data[data['Source?'] == 'No'].copy()
     ndata = ndata[['ID', 'Sites Locations']]
-    return _add_index_as_col(Geolocation.transform(ndata), 'link_id')
+    tlogg.info(
+        f"Querying GoogleAPI for sitelocation information.")
+    ndata = _add_index_as_col(Geolocation.transform(ndata), 'link_id')
+    ndata.to_csv('site_locations_test.csv') ## DEBUG
+    return ndata
