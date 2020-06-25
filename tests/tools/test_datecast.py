8cb47bd28b898f5d925bc62932b065d45506fd16
newnativeabq <vincent.a.brandon@gmail.com>
2020-06-25 17:47:16 -0600

    FIX: Updated datetime validation for valid year, month, day ranges

--- tests/tools/test_datecast.py
+++ tests/tools/test_datecast.py
@@ -8,25 +8,28 @@ from tests.testconfig import get_logger
 
 logger = get_logger(__name__)
 
+
 class DateTimeConversionTest(unittest.TestCase):
     
     def setUp(self):
         self.sdate = '2020/02/02'
-        self.sbaddate = '2020/02/200'
+        self.sbaddates = ['2020/02/200', '202/12/20', '1990/10/35', '5/5/2045']
 
     def test_convert_to_datetime(self):
-        nm = 'BasicConversion: '
+        nm = 'BasicConversion'
         result = convert_to_datetime(self.sdate)
 
-        logger.info(''.join([nm, str(result)]))
+        logger.info('-'.join([nm, str(result)]))
         self.assertEqual('2020', str(result.year))
 
-    def test_catch_bad_day(self):
+    def test_catch_bad_date(self):
         nm = 'BadConversionDay: '
-        result = convert_to_datetime(self.sbaddate)
 
-        logger.info(''.join([nm, str(result)]))
-        self.assertIsNone(result)
+
+        for dst in self.sbaddates:
+            result = convert_to_datetime(dst)
+            logger.info(''.join([nm, str(result)]))
+            self.assertIsNone(result)
 
 
 
